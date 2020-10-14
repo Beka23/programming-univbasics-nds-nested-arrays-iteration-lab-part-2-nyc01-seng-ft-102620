711bf22bf3deee9a59aacc5224b89aba60be287b
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 01:04:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ smallest_numbers = []
 row_index = 0 
 while row_index < src.count do 
   element_index = 0 
-  min_num = src.min 
+  min_num = .min 
   while element_index < src[row_index].count do 
     if src[row_index][element_index] < min_num 
       min_num = src[row_index][element_index]
