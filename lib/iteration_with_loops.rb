27522f82150c1075c5c7d1a05d5e03f7ae05a0e9
Beksultan <beksultankaiypov@gmail.com>
2020-10-14 00:49:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,7 +8,7 @@ while row_index < src.count do
   element_index = 0 
   min_num = 1 
   while element_index < src[row_index].count do 
-    if src[row_index][element_index] > min_num 
+    if src[row_index][element_index] < min_num 
       min_num = src[row_index][element_index]
     end
     element_index += 1 
